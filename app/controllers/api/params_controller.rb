9171e44d59b0d63ccb8ca12967c4c4d0ab4c37fe
Lexa <lexaraea@gmail.com>
2019-06-27 20:09:03 -0400

    make adjustments to several files

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,7 +1,41 @@
 class Api::ParamsController < ApplicationController
   def query
-    p "does this work?"
-    @name = params["name"].reverse
+    # behind the scenes
+    # params = {"name" => "brian"}
+
+    name = params["name"]
+    first_character = params["name"][0]
+    if first_character == 'a'
+      @message = "The first letter of your name is A"
+    else
+      @message = "Your name is #{name}"
+    end
+
     render 'query_parameter.json.jb'
+  end
+
+  def game
+    # get user input
+    the_guess = params["user_guess"].to_i
+    # compare that user input to the actual answer
+    if the_guess == 32
+      @user_feedback = "you win"
+    elsif the_guess > 32
+      @user_feedback = "too high"
+    elsif the_guess < 32
+      @user_feedback = "too low"
+    end
+    # give the user a message based on their guess
+    render 'guess.json.jb'
+  end
+
+  def url_segment_method
+    @name = params["name"]
+    render 'segment_view.json.jb'
+  end
+
+  def body_param_action
+    @name = params[:name]
+    render 'body.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/query_url" => "params#query"
+    get "guessing_game" => "params#game"
+
+  # URL segment parameter
+    get "/segment_parameter_url/:name" => "params#url_segment_method"
+    get "/segment_guessing_game/:user_guess" => "params#game"
+
+  # body parameter
+    post '/body_parameter_url' => 'params#body_param_action'
+  end
 end
